9c42c6908f2cb51e23d6240800d07c7da00b97c2
balaram555 <22a91a0599@aec.edu.in>
2025-08-22 15:26:23 +0530

    Time: 7 ms (65.65%), Space: 45.8 MB (35.71%) - LeetHub

---
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSubmat(int[][] mat) {
+       int n=mat.length;
+       int m=mat[0].length;
+       int[] arr=new int[m];
+       int sum=0;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==0)
+            arr[j]=0;
+            else
+            arr[j]++;
+        }
+        for(int j=0;j<m;j++){
+        int min=arr[j];
+        for(int k=j;k<m;k++){
+           min=Math.min(min,arr[k]);
+           sum+=min;
+        }
+       }
+       } 
+       
+       return sum;
+    }
+}
