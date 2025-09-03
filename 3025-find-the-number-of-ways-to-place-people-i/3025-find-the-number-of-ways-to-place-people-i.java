efdeef8f7b12ab18f3b0a9ffb4b8db64cd89008a
balaram555 <22a91a0599@aec.edu.in>
2025-09-03 10:01:50 +0530

    Time: 5 ms (100%), Space: 44.3 MB (90.79%) - LeetHub

---
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+         int n=points.length;
+        int count=0;
+        Arrays.sort(points, (a, b) -> {
+    if (a[0] != b[0]) {
+        return Integer.compare(a[0], b[0]); 
+    } else {
+        return Integer.compare(b[1], a[1]); 
+    }
+});
+        for(int i=0;i<n;i++){
+            int m = Integer.MIN_VALUE;
+            int max=points[i][1];
+            for(int j=i+1;j<n;j++)
+            {
+                if(points[i][0]<=points[j][0]&&points[i][1]>=points[j][1]){
+                    if (points[j][1] > m && max>m) {
+                        count++;
+                        m = points[j][1];
+                    } 
+                }
+            }
+        }
+        return count;
+    }
+}
