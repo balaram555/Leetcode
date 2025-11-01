65d1612fc3135b797fa107119fcc83047a140456
balaram555 <22a91a0599@aec.edu.in>
2025-11-01 17:31:57 +0530

    Time: 1 ms (56.28%), Space: 45.5 MB (5.95%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -3,12 +3,12 @@ class Solution {
         int n=nums.length;
         int sum=0;
         int c=0;
-       for(int num:nums){
-        if(num>0&&num<10)
-        sum +=num;
-        else if(num>=10&&num<=99)
-        c+=num;
-       }
+       for(int i=0;i<n;i++){
+        if(nums[i]>0&&nums[i]<=9)
+        sum +=nums[i];
+        else if (nums[i] >= 10 && nums[i] <= 99)
+        c +=nums[i];
+       } 
        if(sum==c)
        return false;
        return true;
